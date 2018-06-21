773a3ab51ae60957d55f5ed2ffaac6040bc5360e
Francis John Agunday <jarvis2@Franciss-MacBook-Pro.local>
2018-06-22 09:01:37 +1200

    Updated Runcukestest

--- cucumberselenium2library/src/test/java/cucumbertests/RunCukesTest.java
+++ cucumberselenium2library/src/test/java/cucumbertests/RunCukesTest.java
@@ -7,18 +7,10 @@ import cucumber.api.junit.Cucumber;
 import resources.utils.constants.GlobalVariables;
 
 @RunWith(Cucumber.class)
-// @CucumberOptions(plugin = {"pretty", "com.epam.reportportal.cucumber.ScenarioReporter"})
-
 @CucumberOptions(
-		plugin = {
-			 //"pretty", "html:target/reportportal",
-			 //"html:target/cucumberHtmlReport",     //  for html result
-			 //"pretty:target/cucumber-json-report.json",   // for json result
-			 "pretty", "com.epam.reportportal.cucumber.StepReporter"
-		     },
+		plugin = {"pretty", "com.epam.reportportal.cucumber.StepReporter"},
 		features = GlobalVariables.FEATURES_PATH,
-		glue = {GlobalVariables.GLUE_KEYWORDS_PATH   // predefined step definitions package
-			   }
+		glue = {GlobalVariables.GLUE_KEYWORDS_PATH}
 )
 public class RunCukesTest { 	
 }
